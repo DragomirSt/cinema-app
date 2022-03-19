cf9fa3e3ca9156d88f67ebe2d4c4c99bd196b954
DragomirSt <drago89@abv.bg>
2022-03-19 04:48:29 -0700

    Update movieController.js
    add another route to tv-series which fetch data from the imdb server

--- src/controlers/movieController.js
+++ src/controlers/movieController.js
@@ -3,13 +3,26 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
-router.get('/movies', async (req, res) => {
+router.get('/movies', (req, res) => {
     return fetch(`https://imdb-api.com/en/API/Top250Movies/${process.env.NASA_API_KEY}`)
         .then(res => res.json())
         .then(data => {
 
             const movies = data.items.slice(0, 20);
-            res.render('top-20-movies', { movie: movies })
+            res.render('top-20-movies', { movie: movies });
+        })
+        .catch(err => {
+            throw err;
+        });
+});
+
+router.get('/tv-series', (req, res) => {
+    return fetch(`https://imdb-api.com/en/API/Top250TVs/${process.env.NASA_API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            
+            const tvSeries = data.items.slice(0, 20);
+            res.render('top-20-tv-series', { tvSeries: tvSeries });
         })
         .catch(err => {
             throw err;
