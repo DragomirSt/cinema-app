d0efe198e20299dfda156861ba26721aa12ca721
DragomirSt <drago89@abv.bg>
2022-03-19 05:11:42 -0700

    Update movieController.js
    add new route to coming soon movies based on the data from imdb

--- src/controlers/movieController.js
+++ src/controlers/movieController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const fetch = require('node-fetch');
 
 router.get('/movies', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/Top250Movies/${process.env.NASA_API_KEY}`)
+    return fetch(`https://imdb-api.com/en/API/Top250Movies/${process.env.IMDB_API_KEY}`)
         .then(res => res.json())
         .then(data => {
 
@@ -17,12 +17,25 @@ router.get('/movies', (req, res) => {
 });
 
 router.get('/tv-series', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/Top250TVs/${process.env.NASA_API_KEY}`)
+    return fetch(`https://imdb-api.com/en/API/Top250TVs/${process.env.IMDB_API_KEY}`)
         .then(res => res.json())
         .then(data => {
-            
+
             const tvSeries = data.items.slice(0, 20);
             res.render('top-20-tv-series', { tvSeries: tvSeries });
+        })
+        .catch(err => {
+            throw err;
+        });
+});
+
+router.get('/coming-soon', (req, res) => {
+    return fetch(`https://imdb-api.com/en/API/ComingSoon/${process.env.IMDB_API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+
+            const commingSoon = data.items;
+            res.render('comming-soon', { newReleases: commingSoon });
         })
         .catch(err => {
             throw err;
