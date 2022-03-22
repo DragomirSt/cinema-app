bab785d598c02a199cb69b537489218a743d9467
DragomirSt <drago89@abv.bg>
2022-03-22 00:19:00 -0700

    Update movieController.js
    add more routes with a functionality to fetch data from the IMDB APIs

--- src/controlers/movieController.js
+++ src/controlers/movieController.js
@@ -9,7 +9,7 @@ router.get('/movies', (req, res) => {
         .then(data => {
 
             const movies = data.items.slice(0, 20);
-            res.render('top-20-movies', { movie: movies });
+            res.render('top-20-movies', { title: 'Top 20s Movies', movie: movies });
         })
         .catch(err => {
             throw err;
@@ -22,7 +22,7 @@ router.get('/tv-series', (req, res) => {
         .then(data => {
 
             const tvSeries = data.items.slice(0, 20);
-            res.render('top-20-tv-series', { tvSeries: tvSeries });
+            res.render('top-20-tv-series', { title: 'Top 20s TV Series', tvSeries: tvSeries });
         })
         .catch(err => {
             throw err;
@@ -35,11 +35,52 @@ router.get('/coming-soon', (req, res) => {
         .then(data => {
 
             const commingSoon = data.items;
-            res.render('coming-soon', { newReleases: commingSoon });
+            res.render('coming-soon', { title: 'Coming Soon', newReleases: commingSoon });
         })
         .catch(err => {
             throw err;
         });
+});
+
+router.get('/watch-now', (req, res) => {
+    return fetch(`https://imdb-api.com/en/API/InTheaters/${process.env.IMDB_API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+
+            const inTheaters = data.items;
+            res.render('watch-now', { title: 'In Theatres', watchNow: inTheaters });
+        })
+        .catch(err => {
+            throw err;
+        });
+});
+
+router.get('/trailer/:id', (req, res) => {
+    return fetch(`https://imdb-api.com/en/API/Trailer/${process.env.IMDB_API_KEY}/${req.params.id}`)
+        .then(res => res.json())
+        .then(trailer => {
+            res.redirect(trailer.link);
+        })
+        .catch(err => {
+            throw err;
+        });
+});
+
+router.get('/search', (req, res) => {
+    if (req.query.text !== undefined) {
+        return fetch(`https://imdb-api.com/en/API/SearchTitle/${process.env.IMDB_API_KEY}/${req.query.text}`)
+            .then(res => res.json())
+            .then(movie => {
+
+                const searchResults = movie.results;
+                res.render('search', { title: 'Search IMDB', results: searchResults });
+            })
+            .catch(err => {
+                throw err;
+            });
+    } else {
+        res.render('search');
+    }
 });
 
 module.exports = router;
