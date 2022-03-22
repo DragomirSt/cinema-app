f87dd06bde94e59b652eed1bf2a1250d85e5119f
DragomirSt <drago89@abv.bg>
2022-03-22 00:17:13 -0700

    Update index.js
    add a path to the static folder with the css file

--- src/index.js
+++ src/index.js
@@ -10,4 +10,8 @@ app.use(express.json());
 const router = require('./routes');
 app.use(router);
 
+const path = require('path');
+app.use(express.static(path.resolve(__dirname, './static')));
+app.locals.title = 'Cinema';
+
 app.listen(PORT, console.log(`app is running on http://localhost:${PORT}`));
