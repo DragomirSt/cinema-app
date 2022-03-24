b6102d2c70b18d50649d06b856b92e642b0012d8
DragomirSt <drago89@abv.bg>
2022-03-24 00:49:15 -0700

    add 404 middleware
    made a middleware so the user can see 404 page in case of a route that doesn't exist

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 const handlebars = require('./config/handlebars.js')(app);
 require('dotenv').config();
-app.use(express.json());
 
 const router = require('./routes');
 app.use(router);
@@ -13,5 +12,9 @@ app.use(router);
 const path = require('path');
 app.use(express.static(path.resolve(__dirname, './static')));
 app.locals.title = 'Cinema';
+
+app.use((req, res) => {
+    res.status('404').render('404');
+});
 
 app.listen(PORT, console.log(`app is running on http://localhost:${PORT}`));
