262ee164490e72d7bff3e9d04fbc6e2772bb9263
DragomirSt <drago89@abv.bg>
2022-04-18 05:47:21 -0700

    Update movieController.js
    add one more route to the plot of the movie

--- src/controlers/movieController.js
+++ src/controlers/movieController.js
@@ -9,10 +9,10 @@ router.get('/movies', (req, res) => {
         .then(data => {
 
             const movies = data.items.slice(0, 20);
-            res.render('top-20-movies', { title: 'Top 20s Movies', movie: movies });
+            return res.render('top-20-movies', { title: 'Top 20s Movies', movie: movies });
         })
         .catch(err => {
-            res.status('503').render('503');
+            return res.status('503').render('503');
         });
 });
 
@@ -22,10 +22,10 @@ router.get('/tv-series', (req, res) => {
         .then(data => {
 
             const tvSeries = data.items.slice(0, 20);
-            res.render('top-20-tv-series', { title: 'Top 20s TV Series', tvSeries: tvSeries });
+            return res.render('top-20-tv-series', { title: 'Top 20s TV Series', tvSeries: tvSeries });
         })
         .catch(err => {
-            res.status('503').render('503');
+            return res.status('503').render('503');
         });
 });
 
@@ -35,10 +35,10 @@ router.get('/coming-soon', (req, res) => {
         .then(data => {
 
             const commingSoon = data.items;
-            res.render('coming-soon', { title: 'Coming Soon', newReleases: commingSoon });
+            return res.render('coming-soon', { title: 'Coming Soon', newReleases: commingSoon });
         })
         .catch(err => {
-            res.status('503').render('503');
+            return res.status('503').render('503');
         });
 });
 
@@ -48,10 +48,10 @@ router.get('/watch-now', (req, res) => {
         .then(data => {
 
             const inTheaters = data.items;
-            res.render('watch-now', { title: 'In Theatres', watchNow: inTheaters });
+            return res.render('watch-now', { title: 'In Theatres', watchNow: inTheaters });
         })
         .catch(err => {
-            res.status('503').render('503');
+            return res.status('503').render('503');
         });
 });
 
@@ -62,7 +62,7 @@ router.get('/trailer/:id', (req, res) => {
             res.redirect(trailer.link);
         })
         .catch(err => {
-            res.status('503').render('503');
+            return res.status('503').render('503');
         });
 });
 
@@ -77,11 +77,40 @@ router.get('/search', (req, res) => {
                 res.render('search', { title: 'Search IMDB', result: searchResult, error: errorMessage });
             })
             .catch(err => {
-                res.status('503').render('503');
+                return res.status('503').render('503');
             });
     } else {
-        res.render('search');
+        return res.render('search');
     }
+});
+
+router.get('/movie-plot/:id', async (req, res) => {
+    const details = {
+        img: '',
+        description: '',
+        id: ''
+    };
+
+    try {
+        const data = await fetch(`https://imdb-api.com/en/API/Wikipedia/${process.env.IMDB_API_KEY}/${req.params.id}`);
+        const plot = await data.json();
+        details.description = plot.plotShort.plainText;
+        details.id = plot.imDbId;
+
+    } catch (error) {
+        return res.status('503').render('503');
+    };
+
+    try {
+        const poster = await fetch(`https://imdb-api.com/en/API/Posters/${process.env.IMDB_API_KEY}/${req.params.id}`);
+        const image = await poster.json();
+        details.img = image.posters[0].link;
+
+    } catch (error) {
+        return res.status('503').render('503');
+    };
+
+    return res.render('plot', { title: 'Movie Plot', result: details });
 });
 
 module.exports = router;
