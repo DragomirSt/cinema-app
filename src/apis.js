41ad1264e77ae9ecc4306a85122fcd410f83e20e
DragomirSt <drago89@abv.bg>
2022-04-23 00:27:28 -0700

    Create apis.js
    made an object with all api endpoints

---
+++ src/apis.js
@@ -0,0 +1,13 @@
+
+const apis = {
+    movies: 'https://imdb-api.com/en/API/Top250Movies',
+    series: 'https://imdb-api.com/en/API/Top250TVs',
+    coming: 'https://imdb-api.com/en/API/ComingSoon',
+    now: 'https://imdb-api.com/en/API/InTheaters',
+    trailer: 'https://imdb-api.com/en/API/Trailer',
+    wiki: 'https://imdb-api.com/en/API/Wikipedia',
+    poster: 'https://imdb-api.com/en/API/Posters',
+    search: 'https://imdb-api.com/en/API/SearchTitle'
+}
+
+module.exports = apis;
