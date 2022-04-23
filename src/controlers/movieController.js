7c05c976e928af9194a0c14be0464dc876098741
DragomirSt <drago89@abv.bg>
2022-04-23 00:37:15 -0700

    Update movieController.js
    1. import the apis and fetch function
    2. refactor the code to use them
    3. made a regex to check if the images links are resized

--- src/controlers/movieController.js
+++ src/controlers/movieController.js
@@ -1,87 +1,77 @@
 
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
 
-router.get('/movies', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/Top250Movies/${process.env.IMDB_API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
+const FetchRequest = require('./../fetch');
+const api = require('./../apis');
+const regex = /[V][0-9][\D][UX]+[0-9]+[\D][CR0-9]+[\D][0-9][\D][0-9]+[\D][0-9]+[\D][AL]+/;
 
-            const movies = data.items.slice(0, 20);
-            return res.render('top-20-movies', { title: 'Top 20s Movies', movie: movies });
-        })
-        .catch(err => {
-            return res.status('503').render('503');
+router.get('/movies', async (req, res) => {
+    try {
+        const data = await FetchRequest(`${api.movies}/${process.env.IMDB_API_KEY}`);
+
+        const movies = data.items.slice(0, 20);
+        movies.map(movie => {
+            if (movie.image.match(regex)) {
+                movie.image = movie.image.replace(regex, '');
+            };
         });
+        return res.render('top-20-movies', { title: 'Top 20s Movies', movie: movies });
+
+    } catch (error) {
+        return res.status('503').render('503');
+    };
 });
 
-router.get('/tv-series', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/Top250TVs/${process.env.IMDB_API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
+router.get('/tv-series', async (req, res) => {
+    try {
+        const data = await FetchRequest(`${api.series}/${process.env.IMDB_API_KEY}`);
 
-            const tvSeries = data.items.slice(0, 20);
-            return res.render('top-20-tv-series', { title: 'Top 20s TV Series', tvSeries: tvSeries });
-        })
-        .catch(err => {
-            return res.status('503').render('503');
+        const tvSeries = data.items.slice(0, 20);
+        tvSeries.map(series => {
+            if (series.image.match(regex)) {
+                series.image = series.image.replace(regex, '');
+            };
         });
+        return res.render('top-20-tv-series', { title: 'Top 20s TV Series', tvSeries: tvSeries });
+
+    } catch (error) {
+        return res.status('503').render('503');
+    };
 });
 
-router.get('/coming-soon', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/ComingSoon/${process.env.IMDB_API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
+router.get('/coming-soon', async (req, res) => {
+    try {
+        const data = await FetchRequest(`${api.coming}/${process.env.IMDB_API_KEY}`);
 
-            const commingSoon = data.items;
-            return res.render('coming-soon', { title: 'Coming Soon', newReleases: commingSoon });
-        })
-        .catch(err => {
-            return res.status('503').render('503');
+        const commingSoon = data.items;
+        commingSoon.map(coming => {
+            if (coming.image.match(regex)) {
+                coming.image = coming.image.replace(regex, '');
+            };
         });
+        return res.render('coming-soon', { title: 'Coming Soon', newReleases: commingSoon });
+
+    } catch (error) {
+        return res.status('503').render('503');
+    };
 });
 
-router.get('/watch-now', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/InTheaters/${process.env.IMDB_API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
+router.get('/watch-now', async (req, res) => {
+    try {
+        const data = await FetchRequest(`${api.now}/${process.env.IMDB_API_KEY}`);
 
-            const inTheaters = data.items;
-            return res.render('watch-now', { title: 'In Theatres', watchNow: inTheaters });
-        })
-        .catch(err => {
-            return res.status('503').render('503');
+        const inTheaters = data.items;
+        inTheaters.map(watch => {
+            if (watch.image.match(regex)) {
+                watch.image = watch.image.replace(regex, '');
+            };
         });
-});
+        return res.render('watch-now', { title: 'In Theatres', watchNow: inTheaters });
 
-router.get('/trailer/:id', (req, res) => {
-    return fetch(`https://imdb-api.com/en/API/Trailer/${process.env.IMDB_API_KEY}/${req.params.id}`)
-        .then(res => res.json())
-        .then(trailer => {
-            res.redirect(trailer.link);
-        })
-        .catch(err => {
-            return res.status('503').render('503');
-        });
-});
-
-router.get('/search', (req, res) => {
-    if (req.query.text !== undefined) {
-        return fetch(`https://imdb-api.com/en/API/SearchTitle/${process.env.IMDB_API_KEY}/${req.query.text}`)
-            .then(res => res.json())
-            .then(movie => {
-
-                const searchResult = movie.results[0];
-                const errorMessage = 'Could not find your movie, try again.';
-                res.render('search', { title: 'Search IMDB', result: searchResult, error: errorMessage });
-            })
-            .catch(err => {
-                return res.status('503').render('503');
-            });
-    } else {
-        return res.render('search');
-    }
+    } catch (error) {
+        return res.status('503').render('503');
+    };
 });
 
 router.get('/movie-plot/:id', async (req, res) => {
@@ -92,25 +82,51 @@ router.get('/movie-plot/:id', async (req, res) => {
     };
 
     try {
-        const data = await fetch(`https://imdb-api.com/en/API/Wikipedia/${process.env.IMDB_API_KEY}/${req.params.id}`);
-        const plot = await data.json();
-        details.description = plot.plotShort.plainText;
-        details.id = plot.imDbId;
+        const data = await FetchRequest(`${api.wiki}/${process.env.IMDB_API_KEY}/${req.params.id}`);
+
+        details.description = data.plotShort.plainText;
+        details.id = data.imDbId;
 
     } catch (error) {
         return res.status('503').render('503');
     };
 
     try {
-        const poster = await fetch(`https://imdb-api.com/en/API/Posters/${process.env.IMDB_API_KEY}/${req.params.id}`);
-        const image = await poster.json();
-        details.img = image.posters[0].link;
+        const poster = await FetchRequest(`${api.poster}/${process.env.IMDB_API_KEY}/${req.params.id}`);
+        details.img = poster.posters[0].link;
 
     } catch (error) {
         return res.status('503').render('503');
     };
 
     return res.render('plot', { title: 'Movie Plot', result: details });
+});
+
+router.get('/trailer/:id', async (req, res) => {
+    try {
+        const data = await FetchRequest(`${api.trailer}/${process.env.IMDB_API_KEY}/${req.params.id}`);
+        res.redirect(data.link);
+
+    } catch (error) {
+        return res.status('503').render('503');
+    };
+});
+
+router.get('/search', async (req, res) => {
+    if (req.query.text !== undefined) {
+        try {
+            const data = await FetchRequest(`${api.search}/${process.env.IMDB_API_KEY}/${req.query.text}`);
+
+            const searchResult = data.results;
+            const errorMessage = 'Could not find your movie, try again.';
+            res.render('search', { title: 'Search IMDB', result: searchResult, error: errorMessage });
+
+        } catch (error) {
+            return res.status('503').render('503');
+        };
+    } else {
+        return res.render('search');
+    };
 });
 
 module.exports = router;
